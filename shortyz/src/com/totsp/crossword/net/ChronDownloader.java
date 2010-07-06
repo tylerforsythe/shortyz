89c76b0be99bf5650e75cff335d869709ce0c6ef
supermonkey <supermonkey@gmail.com>
2010-07-06 11:02:36 -0700

    Add Jonesin' Crosswords to puzzle sources.

---
+++ shortyz/src/com/totsp/crossword/net/ChronDownloader.java
@@ -0,0 +1,35 @@
+package com.totsp.crossword.net;
+
+import java.io.File;
+
+import java.text.NumberFormat;
+
+import java.util.Date;
+
+
+public class ChronDownloader extends AbstractDownloader {
+    private static final String NAME = "Houston Chronicle";
+    NumberFormat nf = NumberFormat.getInstance();
+
+    public ChronDownloader() {
+        super("http://www.chron.com/apps/games/xword/puzzles/", DOWNLOAD_DIR,
+            NAME);
+        nf.setMinimumIntegerDigits(2);
+        nf.setMaximumFractionDigits(0);
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public File download(Date date) {
+        return super.download(date, this.createUrlSuffix(date));
+    }
+
+	@Override
+	protected String createUrlSuffix(Date date) {
+		return "cs" + (date.getYear() - 100) +
+        nf.format(date.getMonth() + 1) + nf.format(date.getDate()) +
+        ".puz";
+	}
+}
